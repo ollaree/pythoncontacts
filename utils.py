c7db730e26f1f08fafd8cbce9aa3bcc37bae68ff
Matteo Fer <117727414+ollaree@users.noreply.github.com>
2025-02-10 10:56:53 +0100

    Update utils.py
    aggiunta una sola funzione di utils.py

--- utils.py
+++ utils.py
@@ -1,29 +1,4 @@
-# Dizionario per memorizzare i contatti
-contatti = {}
-
-# Funzione per aggiungere un contatto
-def aggiungi_contatto(nome, telefono):
-    if nome not in contatti:
-        contatti[nome] = telefono
-        print(f"Contatto {nome} aggiunto con successo!")
-    else:
-        print(f"Il contatto {nome} esiste già.")
-
-# Funzione per eliminare un contatto
-def elimina_contatto(nome):
-    if nome in contatti:
-        del contatti[nome]
-        print(f"Contatto {nome} eliminato con successo!")
-    else:
-        print(f"Contatto {nome} non trovato.")
-
-# Funzione per cercare un contatto
-def cerca_contatto(nome):
-    if nome in contatti:
-        print(f"Contatto trovato: {nome} - {contatti[nome]}")
-    else:
-        print(f"Contatto {nome} non trovato.")
-
+# 
 # Funzione per vedere tutti i contatti
 def vedi_tutti_contatti():
     if contatti:
